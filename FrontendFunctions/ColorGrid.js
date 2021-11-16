fcbd023fb2613eea3d2bb6336d47b5d1aed07b48
Ashley Kwon <hk216@duke.edu>
2021-11-16 00:54:21 -0500

    html files added

---
+++ FrontendFunctions/ColorGrid.js
@@ -0,0 +1,9 @@
+function setColor(square){
+    var count=1;
+    var chosenColor = document.getElementById('paint');
+    var property = document.getElementById(square);
+    
+    property.style.backgroundColor = chosenColor;
+
+}
+
